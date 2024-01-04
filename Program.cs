634d8314af45f02e6e32719e5b64f7634ad73b95
titoironman <titoironman@msn.com>
2024-01-04 22:03:41 +0300

    Progrm added

--- Program.cs
+++ Program.cs
@@ -9,3 +9,37 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] array1 = { "1234", "1567", "-2", "computer science" };
+        string[] array2 = new string[array1.Length];
+        SecondArray(array1, array2);
+        PrintArray(array2);
+    }
+
+    static void SecondArray(string[] array1, string[] array2)
+    {
+        int count = 0;
+        for (int i = 0; i < array1.Length; i++)
+        {
+            if (array1[i].Length <= 3)
+            {
+                array2[count] = array1[i];
+                count++;
+            }
+        }
+    }
+
+    static void PrintArray(string[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
